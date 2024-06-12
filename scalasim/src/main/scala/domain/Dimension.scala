9eec590a44b0c77209271fd96c34c4b8aefe8936
S-furi <stefano.furi@studio.unibo.it>
2024-06-12 11:34:23 +0200

    chore: specialize dimensions

--- scalasim/src/main/scala/domain/Dimension.scala
+++ scalasim/src/main/scala/domain/Dimension.scala
@@ -1,4 +1,11 @@
 package domain
 
-trait Dimension
-  
+object Dimensions:
+  trait Dimension:
+    def dimensions: Int
+
+  case class TwoDimensionalSpace() extends Dimension:
+    override val dimensions: Int = 2
+
+  case class ThreeDimensionalSpace() extends Dimension:
+    override val dimensions: Int = 3
