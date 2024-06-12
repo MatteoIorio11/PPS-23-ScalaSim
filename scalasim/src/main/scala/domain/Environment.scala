96bf81f2506705a4b46bcf2c380cfda01e8ecdf0
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-12 11:32:57 +0200

    feat: init Environment, add type Matrix, cellularAutomata, getNeighbours start and nextIteration methods

--- scalasim/src/main/scala/domain/Environment.scala
+++ scalasim/src/main/scala/domain/Environment.scala
@@ -1,3 +1,13 @@
 package domain
 
-trait Environment
+import scala.collection.mutable.ArrayBuffer
+
+trait Environment:
+    // -- Attributes
+    type Matrix = Array[Array[Cell[D <: Dimension]]]
+    def cellularAutomata: CellularAutomata
+
+    // -- Methods
+    def getNeighbours(cell: Cell[D <: Dimension]): List[Cell[D <: Dimension]]
+    def start(): Unit
+    def nextIteration(): Unit
