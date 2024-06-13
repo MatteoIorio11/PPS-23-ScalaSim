127f1fd99873116f00828a2204bb783ce8b59238
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-13 17:37:09 +0200

    feat: add Environment object and move the trait inside it

--- scalasim/src/main/scala/domain/Environment.scala
+++ scalasim/src/main/scala/domain/Environment.scala
@@ -2,13 +2,12 @@ package domain
 
 import scala.collection.mutable.ArrayBuffer
 import domain.Dimensions.*
+import domain.CellularAutomata.*
 
-trait Environment[D <: Dimension]:
-    // -- Attributes
-    type Matrix
-    def cellularAutomata: CellularAutomata[D]
-
-    // -- Methods
-    def getNeighbours(cell: Cell[D]): List[Cell[D]]
-    def start(): Unit
-    def nextIteration(): Unit
+object Environment:
+    trait Environment[D <: Dimension]:
+        type Matrix
+        def cellularAutomata: CellularAutomata[D]
+        def getNeighbours(cell: Cell[D]): List[Cell[D]]
+        def start(): Unit
+        def nextIteration(): Unit
