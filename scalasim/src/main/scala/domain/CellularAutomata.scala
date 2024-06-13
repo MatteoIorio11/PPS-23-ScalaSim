695e39634ad6419f0b1955b6230b8fd69244f938
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-13 15:52:22 +0200

    feat: add rule collection

--- scalasim/src/main/scala/domain/CellularAutomata.scala
+++ scalasim/src/main/scala/domain/CellularAutomata.scala
@@ -1,8 +1,21 @@
 
 import domain.Dimensions.*
+import domain.NeighborRule.*
+import java.awt.Dimension
+
 trait CellularAutomata[D: Dimension]
     type Rules
+    def ruleCollection: Rules
     def dimension: D
 
-    def applyRule(cell: Cell[D], neighbours: List[Cell[D]]): Cell
+    def applyRule(cell: Cell[D], neighbours: List[Cell[D]]): Cell[D]
     def getNeighbours(cell: Cell[D]): List[Cell[D]]
+
+
+class CellularAutomata2D[TwoDimensionalSpace](private val dimension: TwoDimensionalSpace) extends CellularAutomata[TwoDimensionalSpace]:
+    override type Rules = Map[CellState.State, NeighborRule]
+    val ruleCollection: Rules = Map()
+
+    def applyRule(cell: Cell[TwoDimensionalSpace], neighbours: List[Cell[TwoDimensionalSpace]]): Cell[TwoDimensionalSpace] = ???
+    def getNeighbours(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
+    def getRules(): Rules = ???
