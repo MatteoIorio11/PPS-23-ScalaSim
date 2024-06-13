3722d6ba34c314e6be403f9bfc02f6ce35eae13b
S-furi <stefano.furi@studio.unibo.it>
2024-06-13 15:24:54 +0200

    chore: remove unused method

--- scalasim/src/main/scala/domain/Dimension.scala
+++ scalasim/src/main/scala/domain/Dimension.scala
@@ -8,7 +8,6 @@ object Dimensions:
 
   case class TwoDimensionalSpace() extends Dimension:
     override val dimensions: Int = 2
-    def euckideanDistance(p1: Position[TwoDimensionalSpace], p2: Position[TwoDimensionalSpace]): Int
 
   case class ThreeDimensionalSpace() extends Dimension:
     override val dimensions: Int = 3
