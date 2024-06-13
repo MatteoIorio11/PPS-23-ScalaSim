bb039e6de5b3f1c1a984d87c7ed8f181f6bfc224
S-furi <stefano.furi@studio.unibo.it>
2024-06-13 17:17:58 +0200

    chore: add 3D position tests

--- scalasim/src/test/scala/domain/PositionTest.scala
+++ scalasim/src/test/scala/domain/PositionTest.scala
@@ -2,7 +2,7 @@ package domain
 
 import org.scalatest.matchers.should.Matchers.*
 
-import domain.Neighbor.Position.Position2D
+import domain.Neighbor.Position.*
 import domain.Neighbor.Position
 
 class PositionTest extends org.scalatest.funsuite.AnyFunSuite:
@@ -11,3 +11,16 @@ class PositionTest extends org.scalatest.funsuite.AnyFunSuite:
     val p: Position2D = Position2D((10, 20))
     p.coordinates shouldBe (10, 20)
 
+  test("Three dimensional position should be created properly"):
+    val p: Position3D = Position3D((10, 20, 30))
+    p.coordinates shouldBe (10, 20, 30)
+
+  test("Generic position construction with two coordinates should map a Postion2D"):
+    val p = Position((10, 20))
+    p.coordinates shouldBe (10, 20)
+    p.isInstanceOf[Position2D] shouldBe true
+
+  test("Generic position construction with three coordinates should map a Postion3D"):
+    val p = Position((10, 20, 30))
+    p.coordinates shouldBe (10, 20, 30)
+    p.isInstanceOf[Position3D] shouldBe true
