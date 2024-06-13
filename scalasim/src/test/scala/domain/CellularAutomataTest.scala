7d03464dde2aec929e8f821cc1e67eeedc4f6485
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-13 17:03:52 +0200

    feat: add imports

--- scalasim/src/test/scala/domain/CellularAutomataTest.scala
+++ scalasim/src/test/scala/domain/CellularAutomataTest.scala
@@ -2,9 +2,12 @@ package domain
 
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers.*
+import domain.Dimensions.*
+import domain.CellularAutomata.CellularAutomata
+import domain.CellularAutomata2D
 
 class CellularAutomataTest extends AnyFlatSpec:
   val cellularAutomata2D: CellularAutomata[TwoDimensionalSpace] = CellularAutomata2D[TwoDimensionalSpace]()
 
   test("Cellular Automata's map rule should be empty"):
-    ???
+    this.cellularAutomata2D.getRules should be (Map())
