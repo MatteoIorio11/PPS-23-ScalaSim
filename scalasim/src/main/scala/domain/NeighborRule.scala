481fb4349c4ff30c78a25008deb37b6d328bdf1f
S-furi <stefano.furi@studio.unibo.it>
2024-06-13 15:45:24 +0200

    chore: implement position

--- scalasim/src/main/scala/domain/NeighborRule.scala
+++ scalasim/src/main/scala/domain/NeighborRule.scala
@@ -7,13 +7,16 @@ object Neighbor:
     def coordinates: Tuple
 
   object Position:
-    def apply(d: Dimension) = d.dimensions match
-      case 2 => ???
-      case 3 => ???
+    def apply(coordinates: Tuple): Position[? <: Dimension] = coordinates match
+      case x: (Int, Int) => Position2D(x)
+      case x: (Int, Int, Int) => Position3D(x)
       case _ => ???
     
-    class Position2D(private val _coordinates: Tuple2[Int, Int]) extends Position[TwoDimensionalSpace]:
-      def coordinates: Tuple2[Int, Int] = ???
+    class Position2D(private val _coordinates: (Int, Int)) extends Position[TwoDimensionalSpace]:
+      override def coordinates: (Int, Int) = _coordinates
+
+    class Position3D(private val _coordinates: (Int, Int, Int)) extends Position[ThreeDimensionalSpace]:
+      override def coordinates: (Int, Int, Int) = _coordinates
 
 
   trait NeighborRule[D <: Dimension]:
