12bd4b95d09d0c24949745e9f6fad2a9e6738ab5
S-furi <stefano.furi@studio.unibo.it>
2024-06-13 17:01:48 +0200

    fix: fix test

--- scalasim/src/test/scala/domain/PositionTest.scala
+++ scalasim/src/test/scala/domain/PositionTest.scala
@@ -2,9 +2,12 @@ package domain
 
 import org.scalatest.matchers.should.Matchers.*
 
-import domain.Neighbor.*
+import domain.Neighbor.Position.Position2D
+import domain.Neighbor.Position
 
 class PositionTest extends org.scalatest.funsuite.AnyFunSuite:
 
   test("Two dimensional position should be created properly"):
-    val p: Position2D = Position((10, 20, 30))
+    val p: Position2D = Position2D((10, 20))
+    p.coordinates shouldBe (10, 20)
+
