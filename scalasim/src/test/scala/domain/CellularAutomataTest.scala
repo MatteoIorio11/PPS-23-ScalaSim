31c55c59f24d6893317ddea64e747787cdb73a17
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-13 17:16:38 +0200

    fix: imports for scala test

--- scalasim/src/test/scala/domain/CellularAutomataTest.scala
+++ scalasim/src/test/scala/domain/CellularAutomataTest.scala
@@ -1,13 +1,12 @@
 package domain
 
-import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.*
 import domain.Dimensions.*
-import domain.CellularAutomata.CellularAutomata
-import domain.CellularAutomata2D
+import domain.CellularAutomata.*
 
-class CellularAutomataTest extends AnyFlatSpec:
-  val cellularAutomata2D: CellularAutomata[TwoDimensionalSpace] = CellularAutomata2D[TwoDimensionalSpace]()
+class CellularAutomataTest extends AnyFunSuite:
+  val cellularAutomata2D: CellularAutomata[TwoDimensionalSpace] = CellularAutomata2D()
 
   test("Cellular Automata's map rule should be empty"):
     this.cellularAutomata2D.getRules should be (Map())
