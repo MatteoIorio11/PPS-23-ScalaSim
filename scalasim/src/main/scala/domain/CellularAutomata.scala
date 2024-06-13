e567d6316cca7a6b4ae428332750d7837e5fb53c
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-13 17:04:25 +0200

    feat: create CellularAutomata object, shift trait inside the object. Set the class as case class

--- scalasim/src/main/scala/domain/CellularAutomata.scala
+++ scalasim/src/main/scala/domain/CellularAutomata.scala
@@ -3,18 +3,18 @@ package domain
 import domain.Dimensions.*
 import domain.Neighbor.*
 import domain.CellState.*
+object CellularAutomata:
+    trait CellularAutomata[D <: Dimension]:
+        type Rules
+        def ruleCollection: Rules
+        def dimension: D
 
-trait CellularAutomata[D <: Dimension]:
-    type Rules
-    def ruleCollection: Rules
-    def dimension: D
+        def applyRule(cell: Cell[D], neighbours: List[Cell[D]]): Cell[D]
+        def getNeighbours(cell: Cell[D]): List[Cell[D]]
+        def getRules: Rules
 
-    def applyRule(cell: Cell[D], neighbours: List[Cell[D]]): Cell[D]
-    def getNeighbours(cell: Cell[D]): List[Cell[D]]
-    def getRules: Rules
-
-
-class CellularAutomata2D() extends CellularAutomata[TwoDimensionalSpace]:
+import CellularAutomata.CellularAutomata
+case class CellularAutomata2D() extends CellularAutomata[TwoDimensionalSpace]:
     override type Rules = Map[CellState.State, NeighborRule[TwoDimensionalSpace]]
     override val ruleCollection: Rules = Map()
     override val dimension: TwoDimensionalSpace = TwoDimensionalSpace()
