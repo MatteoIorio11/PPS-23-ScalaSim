e56ae41f6ba3c6251953d9c448bbae94cee5a9e7
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-13 17:16:59 +0200

    feat: move the class inside the object

--- scalasim/src/main/scala/domain/CellularAutomata.scala
+++ scalasim/src/main/scala/domain/CellularAutomata.scala
@@ -8,17 +8,15 @@ object CellularAutomata:
         type Rules
         def ruleCollection: Rules
         def dimension: D
-
         def applyRule(cell: Cell[D], neighbours: List[Cell[D]]): Cell[D]
         def getNeighbours(cell: Cell[D]): List[Cell[D]]
         def getRules: Rules
 
-import CellularAutomata.CellularAutomata
-case class CellularAutomata2D() extends CellularAutomata[TwoDimensionalSpace]:
-    override type Rules = Map[CellState.State, NeighborRule[TwoDimensionalSpace]]
-    override val ruleCollection: Rules = Map()
-    override val dimension: TwoDimensionalSpace = TwoDimensionalSpace()
+    case class CellularAutomata2D() extends CellularAutomata[TwoDimensionalSpace]:
+        override type Rules = Map[CellState.State, NeighborRule[TwoDimensionalSpace]]
+        override val ruleCollection: Rules = Map()
+        override val dimension: TwoDimensionalSpace = TwoDimensionalSpace()
 
-    override def applyRule(cell: Cell[TwoDimensionalSpace], neighbours: List[Cell[TwoDimensionalSpace]]): Cell[TwoDimensionalSpace] = ???
-    override def getNeighbours(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
-    override def getRules: Rules = ???
+        override def applyRule(cell: Cell[TwoDimensionalSpace], neighbours: List[Cell[TwoDimensionalSpace]]): Cell[TwoDimensionalSpace] = ???
+        override def getNeighbours(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
+        override def getRules: Rules = ???
