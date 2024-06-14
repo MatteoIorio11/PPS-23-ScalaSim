7a0137eac772fab8e418f36030da17fdcb041cfd
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-14 15:34:54 +0200

    refactor: cell's import, extends cellular automata with cell

--- scalasim/src/main/scala/domain/CellularAutomata.scala
+++ scalasim/src/main/scala/domain/CellularAutomata.scala
@@ -2,7 +2,7 @@ package domain
 
 import domain.Dimensions.*
 import domain.Neighbor.*
-import domain.CellState.*
+import domain.Cell.*
 object CellularAutomata:
     trait CellularAutomata[D <: Dimension]:
         type Rules
@@ -11,12 +11,17 @@ object CellularAutomata:
         def applyRule(cell: Cell[D], neighbours: List[Cell[D]]): Cell[D]
         def getNeighbours(cell: Cell[D]): List[Cell[D]]
         def getRules: Rules
+        def addRule(cellState: State, neighborRule: NeighborRule[D]): Unit
 
-    case class CellularAutomata2D() extends CellularAutomata[TwoDimensionalSpace]:
-        override type Rules = Map[CellState.State, NeighborRule[TwoDimensionalSpace]]
+    case class CellularAutomata2D() extends CellularAutomata[TwoDimensionalSpace] with Cell[TwoDimensionalSpace]:
+        enum State:
+          case Alive
+          case Dead
+          case StandBy
+        override type Rules = Map[State, NeighborRule[TwoDimensionalSpace]]
         override val ruleCollection: Rules = Map()
         override val dimension: TwoDimensionalSpace = TwoDimensionalSpace()
-
         override def applyRule(cell: Cell[TwoDimensionalSpace], neighbours: List[Cell[TwoDimensionalSpace]]): Cell[TwoDimensionalSpace] = ???
         override def getNeighbours(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
-        override def getRules: Rules = ???
+        override def getRules: Rules = ruleCollection
+        override def addRule(cellState: State, neighborRule: NeighborRule[TwoDimensionalSpace]): Unit = ???
