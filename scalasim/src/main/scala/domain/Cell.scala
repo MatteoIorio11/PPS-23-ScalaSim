f73d2f3d2b23a85841f7114930a8adc2b0bd040a
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-14 15:34:27 +0200

    feat: move trait inside object

--- scalasim/src/main/scala/domain/Cell.scala
+++ scalasim/src/main/scala/domain/Cell.scala
@@ -2,14 +2,11 @@ package domain
 
 import domain.Dimensions.*
 
-object CellState:
-    /**
-      * Cell state that will represent all the Cell's state for Its execution.
-      */
+object Cell:
     enum State:
         case Alive
         case Dead
 
-trait Cell[D <: Dimension]:
-    def position: D
-    def state: CellState.State
+    trait Cell[D <: Dimension]:
+        def position: D
+        def state: State
