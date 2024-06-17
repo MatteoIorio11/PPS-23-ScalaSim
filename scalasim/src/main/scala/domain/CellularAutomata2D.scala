ce4b2a056edf5b7b6273bef4ca8196d5ca964cbf
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-17 15:51:49 +0200

    feat: init new object for CellularAuotmata2D

---
+++ scalasim/src/main/scala/domain/CellularAutomata2D.scala
@@ -0,0 +1,24 @@
+package domain
+
+import domain.CellularAutomata.*
+import domain.Dimensions.*
+import domain.Neighbor.*
+import domain.Cell.*
+
+object CellularAutomata2D:
+    def apply(dimension: Dimension): CellularAutomata[? <: Dimension] = dimension match
+        case x: TwoDimensionalSpace => CellularAutomata2DImpl()
+        case _ => throw new IllegalArgumentException("Unsupported dimension")
+    
+    import CellularAutomata.*
+    enum CellularState extends State:
+        case ALIVE
+        case DEAD
+    case class CellularAutomata2DImpl() extends CellularAutomata[TwoDimensionalSpace]:
+        type Rules = Map[State, NeighborRule[TwoDimensionalSpace]]
+        override val ruleCollection: Rules = Map()
+        override val dimension: TwoDimensionalSpace = TwoDimensionalSpace()
+        override def applyRule(cell: Cell[TwoDimensionalSpace], neighbours: List[Cell[TwoDimensionalSpace]]): Cell[TwoDimensionalSpace] = ???
+        override def getNeighbours(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
+        override def getRules: Rules = ruleCollection
+        override def addRule(cellState: State, neighborRule: NeighborRule[TwoDimensionalSpace]): Unit = ???
