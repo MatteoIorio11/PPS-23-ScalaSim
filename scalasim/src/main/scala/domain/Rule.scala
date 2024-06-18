dec8dde22a86f4a7a6dfcc37a4fdfa5a2d80395e
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 15:33:35 +0200

    feat: add neightbor rule utility. Inside this object we will add all the utility methods related to the neighborhood

--- scalasim/src/main/scala/domain/Rule.scala
+++ scalasim/src/main/scala/domain/Rule.scala
@@ -5,9 +5,14 @@ import domain.Dimensions.TwoDimensionalSpace
 import domain.Position
 import domain.Cell.Cell
 import domain.Neighbor
+import domain.CellularAutomata.State
 
 trait Rule[I, O]:
    def tFunc(in: I): O
    def applyTransformation(ca: I): O = tFunc(ca)
 
 trait NeighborRule[D <: Dimension] extends Rule[Neighbor[D], Cell[D]]
+
+object NeighborRuleUtility:
+   def getNeighboursWithState[D <: Dimension](state: State, neighbours: Neighbor[D]): List[Cell[D]] = ???
+      
