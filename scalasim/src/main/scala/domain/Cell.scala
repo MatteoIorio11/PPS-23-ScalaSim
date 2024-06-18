dd043bacaf3dbf1e372a4292214f8e457ea63510
S-furi <stefano.furi@studio.unibo.it>
2024-06-18 18:21:35 +0200

    chore: implement Cell3D

--- scalasim/src/main/scala/domain/Cell.scala
+++ scalasim/src/main/scala/domain/Cell.scala
@@ -2,12 +2,13 @@ package domain
 
 import domain.Dimensions.*
 import domain.CellularAutomata.*
-import domain.Position.Position2D
+import domain.Position.{Position2D, Position3D}
 object Cell:
     trait Cell[D <: Dimension]:
         def position: Position[D]
         def state: State
     
-    class Cell2D(val position: Position[TwoDimensionalSpace], val state: State) extends Cell[TwoDimensionalSpace]:
-        require(position != null)
-        require(state != null)
+    class Cell2D(val position: Position2D, val state: State) extends Cell[TwoDimensionalSpace]:
+        require(position != null) ; require(state != null)
+    class Cell3D(val position: Position3D, val state: State) extends Cell[ThreeDimensionalSpace]:
+        require(position != null) ; require(state != null)
