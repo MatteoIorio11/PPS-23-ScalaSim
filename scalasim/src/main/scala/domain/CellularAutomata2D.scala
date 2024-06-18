fb4577c5b47e44a4890044e04adfaa7b58856bba
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 15:09:20 +0200

    refactor: use Rule instead of NeighborRule

--- scalasim/src/main/scala/domain/CellularAutomata2D.scala
+++ scalasim/src/main/scala/domain/CellularAutomata2D.scala
@@ -14,10 +14,10 @@ object CellularAutomata2D:
         case ALIVE
         case DEAD
     case class CellularAutomata2DImpl() extends CellularAutomata[TwoDimensionalSpace]:
-        type Rules = Map[State, NeighborRule[TwoDimensionalSpace]]
+        type Rules = Map[State, Rule[TwoDimensionalSpace]]
         override val ruleCollection: Rules = Map()
         override val dimension: TwoDimensionalSpace = TwoDimensionalSpace()
         override def applyRule(cell: Cell[TwoDimensionalSpace], neighbours: List[Cell[TwoDimensionalSpace]]): Cell[TwoDimensionalSpace] = ???
         override def getNeighbours(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
         override def getRules: Rules = ruleCollection
-        override def addRule(cellState: State, neighborRule: NeighborRule[TwoDimensionalSpace]): Unit = ???
+        override def addRule(cellState: State, neighborRule: Rule[TwoDimensionalSpace]): Unit = ???
