f0c04b90e3ad046201fdece5d4129b2e9d6810e0
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 17:38:38 +0200

    refactor: change import

--- scalasim/src/test/scala/domain/RuleTest.scala
+++ scalasim/src/test/scala/domain/RuleTest.scala
@@ -3,13 +3,13 @@ package domain
 import org.scalatest.matchers.should.Matchers.*
 import domain.Dimensions.TwoDimensionalSpace
 import domain.CellularAutomata.CellularAutomata
-import domain.CellularAutomata2D
+import domain.GameOfLife
 
 class RuleTest extends org.scalatest.funsuite.AnyFunSuite:
 
   test("An identity Rule should return the same automaton"):
     val r: Rule[Any, Any] = x => x
-    val ca = CellularAutomata2D()
+    val ca = GameOfLife()
 
     r.applyTransformation(ca) shouldBe ca
     r.applyTransformation((1234, "1234")) shouldBe (1234, "1234")
