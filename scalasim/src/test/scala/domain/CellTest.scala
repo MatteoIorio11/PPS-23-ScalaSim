ac74a54ca44b18c7c671bc9486f03c87827ff8b3
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 16:19:12 +0200

    feat: set the correct exception to check, add new test creation of a position null and a correct state

--- scalasim/src/test/scala/domain/CellTest.scala
+++ scalasim/src/test/scala/domain/CellTest.scala
@@ -4,11 +4,18 @@ import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.*
 import domain.Dimensions.*
 import domain.Cell.*
+import domain.CellularAutomata.*
+import domain.Position.Position2D
 
 class CellTest extends AnyFunSuite:
+    enum MyState extends State:
+        case TEST
 
     test("Creation of a new Cell with null elements should return an error"):
-        val error = intercept[RuntimeException](Cell2D(null, null))
-        error shouldBe a[RuntimeException]
+        val error = intercept[IllegalArgumentException](Cell2D(null, null))
+        error shouldBe a[IllegalArgumentException]
 
+    test("Creation of a Cell with dimension equals to null and correct state should return an error"):
+        val state: State = MyState.TEST
+        val error = intercept[IllegalArgumentException](Cell2D(null, state))
   
