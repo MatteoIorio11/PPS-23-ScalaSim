85da54e01c629e0c64416a0232aaccb567f5e2ea
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 17:38:29 +0200

    feat: init GameOfLife env

--- scalasim/src/main/scala/domain/Environment.scala
+++ scalasim/src/main/scala/domain/Environment.scala
@@ -6,9 +6,9 @@ import domain.CellularAutomata.*
 import domain.Cell.*
 
 object Environment:
-    trait Environment[D <: Dimension]:
+    trait Environment[D <: Dimension, I, O]:
         type Matrix
-        def cellularAutomata[D, I, O]: CellularAutomata[D, I, O]
+        def cellularAutomata: CellularAutomata[D, I, O]
         def neighboors(cell: Cell[D]): List[Cell[D]]
         def start(): Unit
         def nextIteration(): Unit
@@ -16,7 +16,7 @@ object Environment:
 
 object Environment2D:
     import Environment.*
-    class GameOfLifeEnvironment(var cellularAutomata: CellularAutomata[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]) extends Environment[TwoDimensionalSpace]:
+    class GameOfLifeEnvironment(var cellularAutomata: CellularAutomata[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]) extends Environment[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]:
         override def neighboors(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
         override def start(): Unit = ???
         override def nextIteration(): Unit = ???
