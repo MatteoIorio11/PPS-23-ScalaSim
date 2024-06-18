3da2aa9090fdaa13cc8fc6d0c9f4a0509fb5f1c8
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 16:15:25 +0200

    fix: set position inside cell as Position[D]

--- scalasim/src/main/scala/domain/Cell.scala
+++ scalasim/src/main/scala/domain/Cell.scala
@@ -2,11 +2,12 @@ package domain
 
 import domain.Dimensions.*
 import domain.CellularAutomata.*
+import domain.Position.Position2D
 object Cell:
     trait Cell[D <: Dimension]:
-        def position: D
+        def position: Position[D]
         def state: State
     
-    class Cell2D(val position: TwoDimensionalSpace, val state: State) extends Cell[TwoDimensionalSpace]:
+    class Cell2D(val position: Position2D, val state: State) extends Cell[TwoDimensionalSpace]:
         require(position != null)
         require(state != null)
