9ed0c840c8413a8f93695e9634b1f4a1f35507fe
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 16:10:59 +0200

    feat: create cell2D, add require for the attributes

--- scalasim/src/main/scala/domain/Cell.scala
+++ scalasim/src/main/scala/domain/Cell.scala
@@ -3,7 +3,10 @@ package domain
 import domain.Dimensions.*
 import domain.CellularAutomata.*
 object Cell:
-
     trait Cell[D <: Dimension]:
         def position: D
         def state: State
+    
+    class Cell2D(val position: TwoDimensionalSpace, val state: State) extends Cell[TwoDimensionalSpace]:
+        require(position != null)
+        require(state != null)
