30419cdf149976366d484b89c1283ea0e38fe3c4
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 17:22:57 +0200

    chore: init Environment2D for game of life, add class GameOfLife that extends the Environment

--- scalasim/src/main/scala/domain/Environment.scala
+++ scalasim/src/main/scala/domain/Environment.scala
@@ -8,7 +8,16 @@ import domain.Cell.*
 object Environment:
     trait Environment[D <: Dimension]:
         type Matrix
-        def cellularAutomata[I, O]: CellularAutomata[D, I, O]
-        def getNeighbours(cell: Cell[D]): List[Cell[D]]
+        def cellularAutomata[D, I, O]: CellularAutomata[D, I, O]
+        def neighboors(cell: Cell[D]): List[Cell[D]]
         def start(): Unit
         def nextIteration(): Unit
+
+
+object Environment2D:
+    import Environment.*
+    class GameOfLifeEnvironment(var cellularAutomata: CellularAutomata[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]) extends Environment[TwoDimensionalSpace]:
+        override def neighboors(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
+        override def start(): Unit = ???
+        override def nextIteration(): Unit = ???
+        type Matrix = Array[Array[Cell[TwoDimensionalSpace]]]
