cb5ad551319493085fa7320e6697a54212895a02
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-18 17:38:20 +0200

    refactor: change name into GameOfLife

--- scalasim/src/main/scala/domain/GameOfLife.scala
+++ scalasim/src/main/scala/domain/GameOfLife.scala
@@ -5,14 +5,14 @@ import domain.Dimensions.*
 import domain.Neighbor
 import domain.Cell.*
 
-object CellularAutomata2D:
+object GameOfLife:
     def apply(): CellularAutomata[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]] = 
-        CellularAutomata2DImpl()
+        GameOfLifeImpl()
     
     enum CellularState extends State:
         case ALIVE
         case DEAD
-    case class CellularAutomata2DImpl() extends CellularAutomata[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]:
+    case class GameOfLifeImpl() extends CellularAutomata[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]:
         type Rules = Map[State, Rule[Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]]
         var ruleCollection: Rules = Map()
         override val dimension: TwoDimensionalSpace = TwoDimensionalSpace()
