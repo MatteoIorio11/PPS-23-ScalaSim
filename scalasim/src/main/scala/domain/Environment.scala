0e73a04981bfe2ddd17131d158e30655013f927d
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-19 15:16:10 +0200

    feat: add logic for the initialization of the matrix, use extension methods for the array and initialise the initial matrix with empty cells

--- scalasim/src/main/scala/domain/Environment.scala
+++ scalasim/src/main/scala/domain/Environment.scala
@@ -4,6 +4,9 @@ import scala.collection.mutable.ArrayBuffer
 import domain.Dimensions.*
 import domain.CellularAutomata.*
 import domain.Cell.*
+import scala.util.Random
+import domain.Position.Position2D
+import domain.GameOfLife.*
 
 object Environment:
     trait Environment[D <: Dimension, I, O]:
@@ -12,17 +15,43 @@ object Environment:
         def dimension: Int
         def cellularAutomata: CellularAutomata[D, I, O]
         def neighboors(cell: Cell[D]): List[Cell[D]]
-        def start(): Unit
+        protected def start(): Unit
         def nextIteration(): Unit
 
 
 object GameOfLifeEnvironment:
+    val maxCellsToSpawn = 50
+    val initialCell = Cell(Position2D((-1, -1).toList), CellState.DEAD)
+
     def apply(dimension: Int): GameOfLifeEnvironmentImpl = 
         GameOfLifeEnvironmentImpl(dimension = dimension, cellularAutomata = GameOfLife())
+
+    extension (array: Array[Array[Cell[TwoDimensionalSpace]]])
+        def initializeEmpty2D(dimension: Int): Array[Array[Cell[TwoDimensionalSpace]]] =
+            val array = Array.fill(dimension, dimension)(initialCell)
+            for (y <- 0 until dimension)
+                for (x <- 0 until dimension)
+                    array(x)(y) = Cell(Position2D((x, y).toList), CellState.DEAD)
+            array
+
+        def initialiseAliveCells(nCells: Int, dimension: Int): Unit = 
+            var usedPositions = Set[Position[TwoDimensionalSpace]]()
+            for cell <- 0 until nCells
+                x = Random.nextInt(dimension)
+                y = Random.nextInt(dimension)
+                position = Position2D((x, y).toList)
+                if !usedPositions.contains(position)
+            yield
+                array(x)(y) = Cell(position, CellState.ALIVE)
+                usedPositions = usedPositions.incl(position)
+
     import Environment.*
     class GameOfLifeEnvironmentImpl(val dimension: Int, val cellularAutomata: CellularAutomata[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]) extends Environment[TwoDimensionalSpace, Neighbor[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]:
-        override def matrix: Matrix = ???
-        override def neighboors(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
-        override def start(): Unit = ???
-        override def nextIteration(): Unit = ???
         type Matrix = Array[Array[Cell[TwoDimensionalSpace]]]
+        def matrix: Matrix = Array.ofDim[Array[Cell[TwoDimensionalSpace]]](dimension).initializeEmpty2D(dimension = dimension)
+        override def neighboors(cell: Cell[TwoDimensionalSpace]): List[Cell[TwoDimensionalSpace]] = ???
+        override def start(): Unit = 
+            val cells: Int = Random.nextInt(maxCellsToSpawn) + 1
+            matrix.initialiseAliveCells(cells, dimension)
+
+        override def nextIteration(): Unit = ???
