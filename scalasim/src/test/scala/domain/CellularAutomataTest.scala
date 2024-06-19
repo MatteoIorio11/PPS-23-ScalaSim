583f3c4bdedc070f337fd48ccc072fd006f7c78f
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-19 15:10:34 +0200

    refactor: change CellularState into CellState

--- scalasim/src/test/scala/domain/CellularAutomataTest.scala
+++ scalasim/src/test/scala/domain/CellularAutomataTest.scala
@@ -17,13 +17,13 @@ class CellularAutomataTest extends AnyFunSuite with BeforeAndAfterEach:
     gameOfLife.rules should be (Map())
 
   test("Add new rule for the Cellular Automata"):
-    val state: State = CellularState.ALIVE
+    val state: State = CellState.ALIVE
     val neighborRule: NeighborRule[TwoDimensionalSpace] = (x: Neighbor[TwoDimensionalSpace]) => 
-      val y = NeighborRuleUtility.getNeighboursWithState(CellularState.ALIVE, x)
+      val y = NeighborRuleUtility.getNeighboursWithState(CellState.ALIVE, x)
       val caller = x.center
       y.length match
-        case 3 => Cell(caller.position.asPosition[Position2D], CellularState.ALIVE)
-        case y if y <= 2 => Cell(caller.position.asPosition[Position2D], CellularState.DEAD)
-        case x if x > 3 => Cell(caller.position.asPosition[Position2D], CellularState.DEAD)
+        case 3 => Cell(caller.position.asPosition[Position2D], CellState.ALIVE)
+        case y if y <= 2 => Cell(caller.position.asPosition[Position2D], CellState.DEAD)
+        case x if x > 3 => Cell(caller.position.asPosition[Position2D], CellState.DEAD)
     gameOfLife.addRule(state, neighborRule)
     gameOfLife.rules should not be (Map())
