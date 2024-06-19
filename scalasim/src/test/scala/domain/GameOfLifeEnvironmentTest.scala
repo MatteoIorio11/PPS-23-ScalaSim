b0ee2efa8710cd165eb906b37082b496be1570d5
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-19 15:15:20 +0200

    feat: add test initial test for the GameOfLife env

--- scalasim/src/test/scala/domain/GameOfLifeEnvironmentTest.scala
+++ scalasim/src/test/scala/domain/GameOfLifeEnvironmentTest.scala
@@ -3,14 +3,13 @@ package domain
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.matchers.should.Matchers.*
 import domain.GameOfLifeEnvironment.*
+import domain.GameOfLife.*
 import org.scalatest.BeforeAndAfterEach
 
 class GameOfLifeEnvironmentTest extends AnyFunSuite with BeforeAndAfterEach:
     val dimension = 100
     val env = GameOfLifeEnvironment(dimension)
 
-    override protected def beforeEach(): Unit = 
-        env.start()
-    
     test("Initialize an environment should add cells into the matrix"):
+        env.start()
         env.matrix.length should not be 0
