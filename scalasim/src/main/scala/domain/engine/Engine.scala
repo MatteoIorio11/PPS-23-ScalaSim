93593dcbe387bb46287e77712fd1e28ba795bf2b
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-20 17:42:00 +0200

    feat: init Engine and Engine2D

--- scalasim/src/main/scala/domain/engine/Engine.scala
+++ scalasim/src/main/scala/domain/engine/Engine.scala
@@ -3,7 +3,25 @@ package domain.engine
 import domain.Environment.Environment
 import domain.base.Dimensions.Dimension
 import domain.Environment.*
+import domain.base.Dimensions.TwoDimensionalSpace
+import domain.automaton.Neighbour
+import domain.automaton.Cell
 
-trait Engine[D <: Dimension, I, O]:
-    def environment: Environment[D, I, O]
-    def nextIteration: Matrix
+object Engine:
+    trait Engine[D <: Dimension, I, O]:
+        def environment: Environment[D, I, O]
+        def nextIteration: Unit
+        def currentState: Matrix
+
+object Engine2D:
+    import Engine.*
+    private case class Engine2D(
+        val environment: Environment[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]])
+     extends Engine[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]:
+
+        override def nextIteration: Unit = ??? 
+
+        override def currentState: Matrix = ???
+
+
+
