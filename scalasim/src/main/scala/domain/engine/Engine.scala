378f95500d23fb0c7d48b258b945bbdd8a3a0ea2
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-20 18:22:52 +0200

    feat: create apply for engine2D

--- scalasim/src/main/scala/domain/engine/Engine.scala
+++ scalasim/src/main/scala/domain/engine/Engine.scala
@@ -6,6 +6,7 @@ import domain.Environment.*
 import domain.base.Dimensions.TwoDimensionalSpace
 import domain.automaton.Neighbour
 import domain.automaton.Cell
+import domain.automaton.NeighborRuleUtility
 
 object Engine:
     trait Engine[D <: Dimension, I, O]:
@@ -17,7 +18,13 @@ object Engine:
 
 object Engine2D:
     import Engine.*
-    private case class Engine2D(
+    val iterations: Int = 1000
+
+    def apply(environment: Environment[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]):
+         Engine[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]] =
+        Engine2DImpl(environment)
+
+    private case class Engine2DImpl(
         val environment: Environment[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]])
      extends Engine[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]:
 
