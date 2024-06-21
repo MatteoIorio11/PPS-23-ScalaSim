97952e74a310b1b7f8d4c8a4206693c43e6b36b8
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-21 15:41:05 +0200

    chore: add partial implementation for start and stop, refactor class name

--- scalasim/src/main/scala/domain/engine/Engine.scala
+++ scalasim/src/main/scala/domain/engine/Engine.scala
@@ -7,11 +7,12 @@ import domain.base.Dimensions.TwoDimensionalSpace
 import domain.automaton.Neighbour
 import domain.automaton.Cell
 import domain.automaton.NeighborRuleUtility
+import domain.simulations.gameoflife.GameOfLifeEnvironment
 
 object Engine:
     trait Engine[D <: Dimension, I, O]:
         type Matrix = Environment[D, I, O]#Matrix
-        protected def running: Boolean
+        def running: Boolean
         def nextIteration: Unit
         def currentState: Matrix
         def start: Unit
@@ -23,13 +24,16 @@ object Engine2D:
 
     def apply(environment: Environment[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]):
          Engine[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]] =
-        Engine2DImpl(environment)
+        SimulationEngine2D(environment)
 
-    private case class Engine2DImpl(
+    private case class SimulationEngine2D(
         val environment: Environment[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]])
      extends Engine[TwoDimensionalSpace, Neighbour[TwoDimensionalSpace], Cell[TwoDimensionalSpace]]:
         var running = false
-        override def nextIteration: Unit = ??? 
+        override def nextIteration: Unit = ???
+            
         override def currentState: Matrix = ???
-        override def stop: Unit = ???
-        override def start: Unit = ???
+        override def stop: Unit = 
+            running = false
+        override def start: Unit = 
+            running = true
