f228dd3fd91011d2944bd3c80ab366983c7bd463
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-23 16:08:51 +0200

    fix: test for new env state

--- scalasim/src/test/scala/domain/EngineTest.scala
+++ scalasim/src/test/scala/domain/EngineTest.scala
@@ -5,6 +5,7 @@ import org.scalatest.matchers.should.Matchers.*
 import domain.engine.Engine2D
 import domain.simulations.gameoflife.GameOfLifeEnvironment
 import utility.DummyAutomatonEnvironment
+import domain.simulations.gameoflife.GameOfLifeEnvironment.initialCell
 
 class EngineTest extends AnyFunSuite:
     val engine = Engine2D(DummyAutomatonEnvironment(10))
@@ -19,3 +20,14 @@ class EngineTest extends AnyFunSuite:
     test("The initial state of the Simulation should not be empty"):
         val currSimulation = engine.currentMatrix
         currSimulation should not be (Iterator.empty)
+
+    test("Starting the engine should create a new stage for the simulation environment"):
+        val initialState = engine.currentMatrix.flatMap(it => it.map(cell => cell))
+        engine.startEngine
+        Thread.sleep(100)
+        val newState = engine.currentMatrix.flatMap(it => it.map(cell => cell))
+        engine.stopEngine
+        initialState.collect {
+            case cell => (cell, newState.filter(c => c.position == cell.position).head)
+        }.forall(x => x._1.state != x._2.state) shouldBe (true)
+
