4ad002d9a71ceac271a0e94ff05ffbb50d6373b4
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-26 15:14:56 +0200

    chore(mixin): implement nextIteration inside IterableEngine, use a Virtual Thread inside the ThreadEngine and add trait's doc

--- scalasim/src/main/scala/domain/engine/Engine.scala
+++ scalasim/src/main/scala/domain/engine/Engine.scala
@@ -11,6 +11,9 @@ import domain.automaton.NeighborRuleUtility
 import domain.simulations.gameoflife.GameOfLifeEnvironment
 import domain.base.Position
 import scala.collection.mutable.ArrayBuffer
+import domain.engine.Engine.IterableThreadEngine2D
+import domain.engine.Engine.IterableTimerEngine2D
+import domain.engine.Engine.IterableEngine2D
 
 object Engine:
     /**
@@ -23,10 +26,11 @@ object Engine:
       */
     trait Engine[D <: Dimension, R]:
         var running: Boolean
-        def history: LazyList[R]
+        var history: LazyList[R]
+        def env: Environment[D]
         protected def environment(): Environment[D]
         protected def nextIteration: Unit
-        protected def saveInHistory: Unit
+        protected def saveInHistory: Unit = history = history:+currentMatrix
         def currentMatrix: R
         def startEngine: Unit
         def stopEngine: Unit
@@ -35,29 +39,34 @@ object Engine:
       * [[Iterable[Iterable[Cell[TwoDimensionalSpace]]]]].
       */
     trait IterableEngine2D extends Engine[TwoDimensionalSpace, Iterable[Iterable[Cell[TwoDimensionalSpace]]]]:
-        override def history: LazyList[Iterable[Iterable[Cell[TwoDimensionalSpace]]]]
-        override def currentMatrix: Iterable[Iterable[Cell[TwoDimensionalSpace]]]
+        var history: LazyList[Iterable[Iterable[Cell[TwoDimensionalSpace]]]]
+        override def currentMatrix: Iterable[Iterable[Cell[TwoDimensionalSpace]]] = 
+            environment().currentMatrix.asInstanceOf[Iterable[Iterable[Cell[TwoDimensionalSpace]]]]
+        override def nextIteration: Unit = 
+            environment().matrix.asInstanceOf[Iterable[Iterable[Cell[TwoDimensionalSpace]]]]
+                    .flatMap(iterable => iterable.map(cell => cell))
+                    .map(cell => env.applyRule(cell, env.neighbours(cell)))
+            saveInHistory
     /**
-      * 
+      * This trait represent a specific type of 2D engine that runs on a Virtual Thread.
       */
     trait IterableThreadEngine2D extends Thread with IterableEngine2D:
-        def env: Environment[TwoDimensionalSpace]
         override protected def environment(): Environment[TwoDimensionalSpace] = 
             this.synchronized:
                 env
         override def startEngine: Unit =
             if (!running)
                 running = true
-                start()
+                Thread.ofVirtual().start(() => this.run())
     /**
-      * 
+      * This trait represent a timed 2D Engine, in which the simulation should runs for the input
+      * timer seconds, and after that the simulation will stop It's execution.
       */
     trait IterableTimerEngine2D extends IterableEngine2D:
         def timer: Int
         private val ONE_SECOND = 1_000
         protected def startTimer: Unit = 
             var currentTimer = 0
-            running = true
             while (currentTimer < timer && running) do 
                 saveInHistory
                 nextIteration
@@ -67,9 +76,6 @@ object Engine:
 
 object Engine2D:
     import Engine.*
-    
-    val iterations: Int = 1000
-
     def apply(environment: Environment[TwoDimensionalSpace],
         tick: Int):
          Engine[TwoDimensionalSpace, Iterable[Iterable[Cell[TwoDimensionalSpace]]]] =
@@ -78,27 +84,24 @@ object Engine2D:
     private case class SimulationEngine2D(val env: Environment[TwoDimensionalSpace], private val tick: Int) extends IterableThreadEngine2D:
         require(tick >= 100)
         @volatile var running = false
-        val dimension = env.dimension
         var history = LazyList()
-        override def nextIteration: Unit = 
-            environment().matrix.asInstanceOf[Iterable[Iterable[Cell[TwoDimensionalSpace]]]]
-                    .flatMap(iterable => iterable.map(cell => cell))
-                    .map(cell => env.applyRule(cell, env.neighbours(cell)))
-            saveInHistory
-        override protected def saveInHistory: Unit = 
-            history = history:+(currentMatrix)
-        override def currentMatrix: Iterable[Iterable[Cell[TwoDimensionalSpace]]] = 
-            environment().currentMatrix.asInstanceOf[Iterable[Iterable[Cell[TwoDimensionalSpace]]]]
         override def stopEngine: Unit = 
             running = false
-        override def startEngine: Unit = 
-            if (!running)
-                running = true
-                start()
         override def run(): Unit = 
             saveInHistory
             while (running)
                 nextIteration
                 Thread.sleep(tick)
-    // private case class TimerEngine2D(val env: Environment[TwoDimensionalSpace], val timer: Int) 
-    //     extends IterableThreadEngine2D with IterableTimerEngine2D
+object TimerEngine2D:
+    import Engine.*
+    def apply(env: Environment[TwoDimensionalSpace], timer: Int): Engine[TwoDimensionalSpace, Iterable[Iterable[Cell[TwoDimensionalSpace]]]] =
+         TimerEngine2D(env, timer)
+    private case class TimerEngine2D(val env: Environment[TwoDimensionalSpace], val timer: Int) 
+    extends IterableThreadEngine2D with IterableTimerEngine2D:
+      require(timer >= 0)
+      var running = false
+      var history: LazyList[Iterable[Iterable[Cell[TwoDimensionalSpace]]]] = LazyList()
+      override def stopEngine: Unit = 
+        running = false
+      override def run(): Unit = 
+        startTimer
