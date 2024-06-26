e3c53bfb1af55b9510ef65a2fec842e9097414ed
S-furi <stefano.furi@studio.unibo.it>
2024-06-26 18:59:38 +0200

    refactor(dsl): move `CustomNeighbourhoodBuilder` into its own file

--- scalasim/src/main/scala/dsl/automaton/NeighbourRule2DBuilder.scala
+++ scalasim/src/main/scala/dsl/automaton/NeighbourRule2DBuilder.scala
@@ -96,86 +96,8 @@ trait NeighbourRule2DBuilder:
      */
     def configureAnother(s: State)(config: NeighbourRule2DBuilder ?=> Unit): NeighbourRule2DBuilder
 
-class CustomNeighbourhoodRuleBuilder extends NeighbourRule2DBuilder:
-
-  private var i: Int = 0
-  private var j: Int = 0
-  // TODO: change access modifiers in order to let tests pass
-  var center: Option[Cell[TwoDimensionalSpace]] = Option.empty
-  var cells: List[Cell[TwoDimensionalSpace]] = List.empty
-  var rules: List[NeighbourRule[TwoDimensionalSpace]] = List.empty
-
-  override def nextRow: this.type =
-    i += 1
-    j = 0
-    this
-
-  override def addCell(s: Option[State]): this.type =
-    s match
-      case Some(state) => cells = cells :+ Cell((i, j).toPosition, state)
-      case _ =>
-    j += 1
-    this
-
-  override def setCenter(s: State): this.type =
-    center = Some(Cell((i, j).toPosition, s))
-    j += 1
-    this
-
-  override def buildRule(s: State): Unit =
-    import domain.automaton.NeighborRuleUtility.*
-
-    rules = rules :+ ((n: Neighbour[TwoDimensionalSpace]) =>
-        val currentRuleToAdjustedPositions = toAbsolutePosition(n.center)
-        if n == currentRuleToAdjustedPositions
-          then Cell(n.center.position, s)
-          else n.center
-        )
-
-  override def relativePositions: List[Cell[TwoDimensionalSpace]] =
-    import domain.automaton.NeighborRuleUtility.-
-
-    center match
-      case Some(c) => cells.map(p => Cell(p.position - c.position, p.state))
-      case _ => throw IllegalStateException("Cannot compute relative positions if center is not defined")
-
-  override def relativeNeighbourhood: Neighbour[TwoDimensionalSpace] =
-    Neighbour(Cell((0, 0).toPosition, center.get.state), relativePositions)
-
-  /**
-   * Builds a [[Neighbour]] based on relative neighbourhood built with the DSL, using [[cntr]] as
-   * a center and with respect to it, computing the absolute coordinates of all the other neighbours.
-   *
-   * @param cntr the center of the neighbourhood
-   * @return a [[Neighbour]] with absolute coordinates based on [[cntr]] as center.
-   */
-  private def toAbsolutePosition(cntr: Cell[TwoDimensionalSpace]): Neighbour[TwoDimensionalSpace] =
-    import domain.automaton.NeighborRuleUtility.{+, -}
-    Neighbour(cntr, relativePositions.map(c => Cell(c.position + cntr.position, c.state)))
-
-  /**
-   * Resets building parameters for this builder, without altering rules list.
-   */
-  private def resetBuild: Unit =
-    i = 0
-    j = 0
-    center = Option.empty
-    cells = List.empty
-
-  override def configureAnother(s: State)(config: NeighbourRule2DBuilder ?=> Unit): NeighbourRule2DBuilder =
-    val otherBuilder = CustomNeighbourhoodRuleBuilder()
-    config(using otherBuilder)
-    otherBuilder.buildRule(s)
-    addRule(otherBuilder.rules(0))
-    this
-
-  private def addRule(nr: NeighbourRule[TwoDimensionalSpace]): Unit = rules = rules :+ nr
-
-  extension (t: (Int, Int))
-    private def toPosition: Position[TwoDimensionalSpace] = Position(t.toList)
-
 object NeighbourRule2DBuilder:
-  export DSL.*
+  export CustomNeighbourhoodDSL.*
 
   def configureRule(s: State)(config: NeighbourRule2DBuilder ?=> Unit): NeighbourRule2DBuilder =
     given builder: CustomNeighbourhoodRuleBuilder = CustomNeighbourhoodRuleBuilder()
@@ -187,7 +109,7 @@ object NeighbourRule2DBuilder:
    * Definition of a Domain Specific Language for configuring a [[NeighbourRule]] making use of
    * [[NeighbourRule2DBuilder]].
    */
-  object DSL:
+  object CustomNeighbourhoodDSL:
     /**
      * Specify an empty cell
      *
