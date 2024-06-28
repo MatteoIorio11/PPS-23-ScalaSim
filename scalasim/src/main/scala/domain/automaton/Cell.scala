2914803b82822743f3994c5c406219e40b2a25dc
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-06-28 11:46:54 +0200

    doc: add scala doc

--- scalasim/src/main/scala/domain/automaton/Cell.scala
+++ scalasim/src/main/scala/domain/automaton/Cell.scala
@@ -13,7 +13,9 @@ import domain.base.Position
 trait Cell[D <: Dimension]:
     def position: Position[D]
     def state: State
-
+/**
+  * Factory for [[Cell]] instances.
+  */
 object Cell:
     def apply[D <: Dimension](p: Position[D], s: State): Cell[D] = CellImpl(p, s)
     private case class CellImpl[D <: Dimension](override val position: Position[D], override val state: State) extends Cell[D]
