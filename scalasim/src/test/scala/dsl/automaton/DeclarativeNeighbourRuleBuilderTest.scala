def9ee50164143a80bc4b801645ba7e117f7fd68
S-furi <stefano.furi@studio.unibo.it>
2024-06-28 11:26:13 +0200

    chore: add test

--- scalasim/src/test/scala/dsl/automaton/DeclarativeNeighbourRuleBuilderTest.scala
+++ scalasim/src/test/scala/dsl/automaton/DeclarativeNeighbourRuleBuilderTest.scala
@@ -18,7 +18,7 @@ class DeclarativeNeighbourRuleBuilderTest extends CustomNeighbourRuleBuilderTest
       alive when atLeastSurroundedBy(2) withState(alive) withRadius(1) whenCenterIs(AnyState)
       alive when atLeastSurroundedBy(2) withState(alive)
 
-  test("The builder should be capable of bulding rules through the DSL"):
+  test("The builder should be capable of bulding a rule through the DSL"):
     val builder = ExpressionRuleBuilder.configureRules:
       alive when atLeastSurroundedBy(2) withState(alive) withRadius(1) whenCenterIs(AnyState)
 
@@ -28,11 +28,11 @@ class DeclarativeNeighbourRuleBuilderTest extends CustomNeighbourRuleBuilderTest
   test("The DSL should map a correct rule"):
     val builder = ExpressionRuleBuilder.configureRules:
       alive when atLeastSurroundedBy(2) withState(alive) withRadius(1) whenCenterIs(AnyState)
-      // alive when atLeastSurroundedBy(2) withState(alive) <----- problems
 
-    val rule = builder.build().head
+    builder.build()
+    val aliveRule = builder.rules.head
 
-    val neighbourhood = Neighbour[TwoDimensionalSpace](
+    val aliveNeighbourhood = Neighbour[TwoDimensionalSpace](
         Cell((1, 0).toPostion, dead),
         List(
           Cell((0, 0).toPostion, alive),
@@ -40,5 +40,23 @@ class DeclarativeNeighbourRuleBuilderTest extends CustomNeighbourRuleBuilderTest
         ),
     )
 
-    val expectedCell = Cell((1, 0).toPostion,  alive)
-    rule.applyTransformation(neighbourhood) shouldBe expectedCell
+    val aliveCell = Cell((1, 0).toPostion,  alive)
+    aliveRule.applyTransformation(aliveNeighbourhood) shouldBe aliveCell
+
+  test("The DSL shoudl map a correct rule even without optional parameters"):
+    val builder = ExpressionRuleBuilder.configureRules:
+      dead when atLeastSurroundedBy(1) withState(dead)
+
+    builder.build()
+    val rule = builder.rules.head
+
+    val n = Neighbour[TwoDimensionalSpace](
+        Cell((1, 0).toPostion, alive),
+        List(
+          Cell((0, 0).toPostion, alive),
+          Cell((2, 0).toPostion, dead),
+        ),
+    )
+
+    val expected = Cell((1, 0).toPostion, dead)
+    rule.applyTransformation(n) shouldBe expected
