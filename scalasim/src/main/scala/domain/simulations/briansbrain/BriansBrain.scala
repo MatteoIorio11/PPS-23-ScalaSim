53b7da75250f8660719d49c85e5377ed143a79f4
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2024-07-02 10:10:15 +0200

    feat: defining BriansBrain eviroment

---
+++ scalasim/src/main/scala/domain/simulations/briansbrain/BriansBrain.scala
@@ -0,0 +1,37 @@
+package domain.simulations.briansbrain
+
+import domain.Environment
+import domain.Environment.Environment
+import domain.automaton.CellularAutomaton.*
+import domain.base.Dimensions.*
+import domain.automaton.{Cell, NeighborRuleUtility, Neighbour, NeighbourRule, Rule}
+import domain.automaton.NeighborRuleUtility.NeighbourhoodLocator
+import domain.automaton.Cell.*
+import domain.base.Position
+import domain.simulations.gameoflife.GameOfLife.CellState
+
+import scala.util.Random
+import scala.collection.mutable.ArrayBuffer
+
+object BriansBrainEnvironment:
+  var maxCellsToSpawn = 0
+  val initialCell: Cell[TwoDimensionalSpace] = Cell(Position(-1, -1), CellState.DEAD)
+
+  def apply(dimension: Int): BriansBrainEnvironmentImpl =
+    maxCellsToSpawn = (dimension / 2) + 1
+    BriansBrainEnvironmentImpl(dimension, cellularAutomata = BriansBrain())
+
+  import Environment.*
+  class BriansBrainEnvironmentImpl(val side: Int, val cellularAutomata: CellularAutomaton[TwoDimensionalSpace])
+    extends Environment[TwoDimensionalSpace] with SquareArrayEnvironment2D:
+    require(side > 0)
+    require(cellularAutomata != null)
+
+    var matrix: Matrix = ArrayBuffer[ArrayBuffer[Cell[TwoDimensionalSpace]]]().initializeSpace(initialCell)
+
+    initialise()
+    override def neighbours(cell: Cell[TwoDimensionalSpace]): Iterable[Cell[TwoDimensionalSpace]] =
+      import domain.automaton.NeighborRuleUtility.given
+      availableCells(circleNeighbourhoodLocator.absoluteNeighboursLocations(cell.position).toList)
+
+    override protected def initialise(): Unit = ???
