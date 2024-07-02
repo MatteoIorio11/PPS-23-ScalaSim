644d01ed3055c0fd2ead8ff6e7bcd16f37a9909a
S-furi <stefano.furi@studio.unibo.it>
2024-07-02 10:16:27 +0200

    docs: add `Rule` docs and some more tests

--- scalasim/src/main/scala/domain/automaton/Rule.scala
+++ scalasim/src/main/scala/domain/automaton/Rule.scala
@@ -8,25 +8,72 @@ import domain.automaton.Neighbour
 import CellularAutomaton.State
 import domain.automaton.NeighborRuleUtility.PositionArithmeticOperations.*
 
+/**
+  * A generic rule for specifying the behaviour of a [[CellularAutomaton]].
+  * A rule is defined by a generic transformation function and an
+  * initial state that this rule must be matched against. The rule
+  * logic then is composed by the a matchin state and the logic
+  * defined inside the input function.
+  * 
+  * @param I the input type for the transformation function.
+  * @param O the output type for the transformation funtcion.
+  */
 trait Rule[I, O]:
+   /**
+     * Generic transformation function that takes an object of type [[I]]
+     * and returns an object of type [[O]].
+     *
+     * @param in the input object of type [[I]].
+     * @return the output object of type [[O]].
+     */
    def tFunc(in: I): O
+   
+   /**
+     * Runner for the transformation function specified.
+     *
+     * @param ca the input object of type [[I]]
+     * @return the output object of type [[O]] after applying the function.
+     */
    def applyTransformation(ca: I): O = tFunc(ca)
+   
+   /**
+     * The [[State]] that this function must match in order to 
+     * proceed to evaluation.
+     *
+     * @return an [[Option]] representing the [[State]] associated to this rule.
+     */
    def matchingState: Option[State] = Option.empty
 
 /**
   * A Neighbor rule is a [[Rule]] based on neighbors' states of a given
-  * cell. Transformation funrctions of this rules should map
-  * a D dimensional [[Neighbour]] into a D dimensional [[Cell]]
+  * cell. Transformation functions of this rules should map
+  * a D dimensional [[Neighbour]] into a D dimensional [[Cell]] with
+  * (hopefully) a mutated state.
   * 
   * @param D the dimension of the space.
   */
 trait NeighbourRule[D <: Dimension] extends Rule[Neighbour[D], Cell[D]]
 
+/**
+  * Companion object for a generic [[NeighbourRule]].
+  */
 object NeighbourRule:
+   /**
+     * Creates a [[D]] dimensional [[NeighbourRule]] that matches the given [[State]] [[s]], and
+     * if the state matches, the transformation function [[f]] can be applied.
+     *
+     * @param state the [[State]] that this rule must match in order to be applied.
+     * @param f the transformation funtcion body that (hopefully) mutates [[Neighbour]] center state.
+     * @return a new [[NeigbourRule]] with the specified behaviour.
+     */
    def apply[D <: Dimension](state: Option[State])(f: Neighbour[D] => Cell[D]): NeighbourRule[D] = new NeighbourRule[D]:
       override def tFunc(n: Neighbour[D]): Cell[D] = f(n)
       override def matchingState: Option[State] = state
 
+/**
+  * Various utility functions for both [[NeighbourRule]]s and [[Neighbour]]s objects.
+  * Most of this methods are used when specifying [[NeighbourRule]]s' behaviours.
+  */
 object NeighborRuleUtility:
    enum RelativePositions(x: Int, y: Int):
       case TopLeft      extends RelativePositions(-1, -1)
@@ -43,23 +90,69 @@ object NeighborRuleUtility:
 
       def toPosition = Position(coordinates.toArray*)
 
+   /**
+     * Utility for configuring a certain neighbourhood placement inside a [[D]] dimensional space.
+     */
    trait NeighbourhoodLocator[D <: Dimension]:
+      /**
+        * 
+        * @return the neighbourhood placement in terms of relative positions with respect to the neighbourhood center.
+        */
       def relativeNeighboursLocations: Iterable[Position[D]]
 
-      def absoluteNeighboursLocations(center: Position[D]): Iterable[Position[D]] =
-         relativeNeighboursLocations.map(c =>
-            center + c
-         )
+      /**
+        * Returns a set of absolute positions (also negative positions) computed on top of the neighbourhood
+        * placement configured with this object, with respect to the provied center.
+        * 
+        * @param center the central [[Position]] of this neighbourhood expressed in absolute position.
+        * @return the neigbours [[Position]]s computed with the provided center position.
+        */
+      def absoluteNeighboursLocations(center: Position[D]): Iterable[Position[D]] = relativeNeighboursLocations.map(c => center + c)
 
+   /**
+     * Collection of arithmetic operations with D dimensional [[Position]]s.
+     */
    object PositionArithmeticOperations:
       extension[D <: Dimension] (p: Position[D])
          private def elementWiseFunc(other: Position[D])(func: (Int, Int) => Int): Position[D] =
             Position(((p.coordinates zip other.coordinates) map { case (a, b) => func(a, b)}).toArray*)
 
+         /**
+           * Elementwise plus operator among two positions inside with a number [[D]] coorindates.
+           *
+           * @param other the other position to apply elementiwise sum.
+           * @return a new position representing the application of elementwise sum.
+           */
          def +(other: Position[D]): Position[D] = elementWiseFunc(other)(_ + _)
+
+         /**
+           * Elementwise minus operator among two positions inside with a number [[D]] coorindates.
+           *
+           * @param other the other position to apply elementiwise minus.
+           * @return a new position representing the application of elementwise minus.
+           */
          def -(other: Position[D]): Position[D] = elementWiseFunc(other)(_ - _)
+
+         /**
+           * Elementwise plus operator among a this position and an integer [[n]].
+           *
+           * @param n the integer used for applying an elementwise sum with this position.
+           * @return a new position representing the application of elementwise sum with an integer.
+           */
+         def +(n: Int): Position[D] = Position(p.coordinates.map(_ + n).toArray*)
+
+         /**
+           * Elementwise minus operator among a this position and an integer [[n]].
+           *
+           * @param n the integer used for applying an elementwise minus with this position.
+           * @return a new position representing the application of elementwise minus with an integer.
+           */
          def -(n: Int): Position[D] = Position(p.coordinates.map(_ - n).toArray*)
 
+   /**
+     * [[NeighbourhoodLocator]] in a two dimensional space representing a full circle of radius one
+     * starting from a central position.
+     */
    given circleNeighbourhoodLocator: NeighbourhoodLocator[TwoDimensionalSpace] = new NeighbourhoodLocator[TwoDimensionalSpace]:
       override def relativeNeighboursLocations: Iterable[Position[TwoDimensionalSpace]] =
          import RelativePositions.*
@@ -74,6 +167,14 @@ object NeighborRuleUtility:
             BottomRight,
             ).map(p => Position(p.coordinates.toArray*))
 
+   /**
+     * Returns a [[NeighbourhoodLocator]] in a two dimensional space representing configured
+     * for handling a circular neighbourhood of the provided radius starting from a central
+     * position.
+     *
+     * @param radius of the neighbourhood.
+     * @return a [[NeighbourhoodLocator]] for handling circular neighbourhoods with a given [[radius]].
+     */
    def getCircularNeighbourhoodPositions(radius: Int = 1): NeighbourhoodLocator[TwoDimensionalSpace] =
       val center = Position(radius, radius)
       var neighbours: List[Position[TwoDimensionalSpace]] = List.empty
@@ -84,5 +185,12 @@ object NeighborRuleUtility:
          override def relativeNeighboursLocations: Iterable[Position[TwoDimensionalSpace]] =
             neighbours.filter(_ != center) map (_ - radius)
 
+   /**
+     * Returns a list of [[D]] dimensional [[Cell]]s inside a [[Neighbour]] with the provided [[State]].
+     *
+     * @param state to be used to match neighbours state.
+     * @param neighbours neighbourhood to look for matching cells with the provided [[State]].
+     * @return the neighbours of this neighbourhood that have the given [[State]].
+     */
    def getNeighboursWithState[D <: Dimension](state: State, neighbours: Neighbour[D]): List[Cell[D]] = 
       neighbours.neighbourhood.filter(cell => cell.state == state)
--- scalasim/src/test/scala/domain/RuleTest.scala
+++ scalasim/src/test/scala/domain/RuleTest.scala
@@ -5,6 +5,14 @@ import domain.base.Dimensions.TwoDimensionalSpace
 import domain.automaton.CellularAutomaton.CellularAutomaton
 import domain.simulations.gameoflife.GameOfLife
 import automaton.Rule
+import domain.automaton.NeighbourRule
+import utility.DummyAutomaton.DummyState.ALIVE
+import utility.DummyAutomaton.DummyState.DEAD
+import domain.automaton.CellularAutomaton.AnyState
+import domain.automaton.Cell
+import domain.base.Position
+import domain.automaton.Neighbour
+import domain.automaton.NeighborRuleUtility.getNeighboursWithState
 
 class RuleTest extends org.scalatest.funsuite.AnyFunSuite:
 
@@ -14,3 +22,35 @@ class RuleTest extends org.scalatest.funsuite.AnyFunSuite:
 
     r.applyTransformation(ca) shouldBe ca
     r.applyTransformation((1234, "1234")) shouldBe (1234, "1234")
+
+  test("A rule can represent any functions"):
+    val r: Rule[Int, String] = x => s"${x} as a string"
+    r.applyTransformation(10) shouldBe "10 as a string"
+
+  test("A basic NeighbourRule should apply transformations"):
+    val invertStateRule: NeighbourRule[TwoDimensionalSpace] = NeighbourRule(Some(AnyState)): n =>
+      n.center.state match
+        case ALIVE => Cell(n.center.position, DEAD)
+        case DEAD => Cell(n.center.position, ALIVE)
+
+    val aliveCell = Cell[TwoDimensionalSpace](Position(0, 0), ALIVE)
+    val deadCell = Cell[TwoDimensionalSpace](Position(0, 0), DEAD)
+
+    invertStateRule.applyTransformation(Neighbour(aliveCell, List.empty)) shouldBe deadCell
+    invertStateRule.applyTransformation(Neighbour(deadCell, List.empty)) shouldBe aliveCell
+
+  test("A NeighbourRule applied to a neighbourhood should apply a transformation"):
+    val nRule: NeighbourRule[TwoDimensionalSpace] = NeighbourRule(Some(ALIVE)): n =>
+      if getNeighboursWithState(DEAD, n).size >= 2  then Cell(n.center.position, DEAD) else n.center
+
+    val center = Cell[TwoDimensionalSpace](Position(1, 1), ALIVE)
+    val neighbourhood: Neighbour[TwoDimensionalSpace] = Neighbour(
+      center,
+      List(
+        Cell(Position(0, 0), DEAD),
+        Cell(Position(0, 2), ALIVE),
+        Cell(Position(2, 1), DEAD),
+      )
+    )
+
+    nRule.applyTransformation(neighbourhood).state shouldBe DEAD
