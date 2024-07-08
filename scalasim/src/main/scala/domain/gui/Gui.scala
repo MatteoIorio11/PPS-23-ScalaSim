c23907fa0c6aa3f3ae0ff3dc7b6cb1de829f9794
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2024-07-08 21:25:06 +0200

    feat: add slider is is toroidal

--- scalasim/src/main/scala/domain/gui/Gui.scala
+++ scalasim/src/main/scala/domain/gui/Gui.scala
@@ -1,7 +1,7 @@
 package domain.gui
 
 import domain.automaton.Cell
-import domain.Environment.GenericEnvironment
+import domain.Environment.{ArrayToroidEnvironment, GenericEnvironment}
 import domain.automaton.CellularAutomaton.State
 import domain.base.Dimensions.{Dimension, TwoDimensionalSpace}
 import domain.engine.Engine.{EngineView, GUIEngine2D}
@@ -15,14 +15,14 @@ import scala.collection.immutable.LazyList
 import domain.simulations.wator.WaTorEnvironment
 import domain.simulations.langtonsant.LangtonsAntEnvironment
 
-case class EnvironmentOption[D <: Dimension, R](name: String, createEnvironment: Int => GenericEnvironment[D,R], colors: Map[State, Color])
+case class EnvironmentOption[D <: Dimension, R](name: String, createEnvironment: (Int, Int) => GenericEnvironment[D, R], colors: Map[State, Color], isToroidal: Boolean)
 
 object EnvironmentOption:
   val options = List(
-    EnvironmentOption("Brian's Brain", BriansBrainEnvironment.apply, BriansBrainEnvironment.colors),
-    EnvironmentOption("Game of Life", GameOfLifeEnvironment.apply(_, 30), GameOfLifeEnvironment.colors),
-    EnvironmentOption("Wa Tor", WaTorEnvironment.apply(_, 10), WaTorEnvironment.colors),
-    EnvironmentOption("Langton's Ant", LangtonsAntEnvironment.apply, LangtonsAntEnvironment.colors)
+    EnvironmentOption("Brian's Brain", (width, height) => BriansBrainEnvironment(width), BriansBrainEnvironment.colors, false),
+    EnvironmentOption("Game of Life", GameOfLifeEnvironment.apply, GameOfLifeEnvironment.colors, true),
+    EnvironmentOption("Wa Tor", WaTorEnvironment.apply, WaTorEnvironment.colors, false),
+    EnvironmentOption("Langton's Ant", (width, height) => LangtonsAntEnvironment(width), LangtonsAntEnvironment.colors, true)
   )
 
 class Gui(val dimension: Tuple2[Int, Int], colors: Map[State, Color]) extends JPanel with EngineView[TwoDimensionalSpace]:
@@ -53,21 +53,23 @@ class Gui(val dimension: Tuple2[Int, Int], colors: Map[State, Color]) extends JP
   val stopButton = JButton("Stop")
   val exitButton = JButton("Exit")
   val comboBox = JComboBox(EnvironmentOption.options.map(_.name).toArray)
-  val sizeSlider = JSlider(10, 200, 100)
+  val widthSlider = JSlider(10, 200, 100)
+  val heightSlider = JSlider(10, 200, 100)
 
   frame.setLayout(null)
   comboBox.setBounds(50, 50, 200, 30)
-  sizeSlider.setBounds(50, 150, 200, 50)
+  widthSlider.setBounds(50, 150, 200, 50)
+  heightSlider.setBounds(50, 200, 200, 50)
   startButton.setBounds(50, 100, 100, 30)
   stopButton.setBounds(150, 100, 100, 30)
   exitButton.setBounds(50, 300, 100, 30)
 
 
   frame.add(comboBox)
-  frame.add(sizeSlider)
   frame.add(startButton)
   frame.add(stopButton)
   frame.add(exitButton)
+  frame.add(widthSlider)
 
   frame.setSize(800, 600)
   frame.setDefaultCloseOperation(3)
@@ -76,20 +78,34 @@ class Gui(val dimension: Tuple2[Int, Int], colors: Map[State, Color]) extends JP
   var guiE: Option[GUIEngine2D] = None
   var currentPanel: Option[Gui] = None
 
+  comboBox.addActionListener(_ =>
+    val selectedOption = comboBox.getSelectedItem.toString
+    val environmentOption = EnvironmentOption.options.find(_.name == selectedOption).get
+
+    if environmentOption.isToroidal then
+      frame.add(heightSlider)
+    else
+      frame.remove(heightSlider)
+
+    frame.revalidate()
+    frame.repaint()
+  )
+
   startButton.addActionListener(e =>
     guiE.foreach(guiEngine => guiEngine.stopEngine)
     currentPanel.foreach(panel => frame.remove(panel))
 
     val selectedOption = comboBox.getSelectedItem.toString
-    val size = sizeSlider.getValue()
-
     val environmentOption = EnvironmentOption.options.find(_.name == selectedOption).get
-    val env = environmentOption.createEnvironment(size)
+    val width = widthSlider.getValue()
+    val height = if environmentOption.isToroidal then heightSlider.getValue() else width
+
+    val env = environmentOption.createEnvironment(width, height)
     val colors = environmentOption.colors
 
-    val pixelPanel = Gui((size, size), colors)
+    val pixelPanel = Gui((width, height), colors)
     val panelSize = 500
-    val pixelSize = panelSize / size
+    val pixelSize = panelSize / math.max(width, height)
     pixelPanel.setPixelSize(pixelSize)
 
     guiE = Some(GUIEngine2D(env, pixelPanel))
