dd19b5948851e56c98ac911b41665a83cd5c23f3
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2024-07-15 15:54:26 +0200

    feat: Add SwingFunctionalFacade

---
+++ scalasim/src/main/scala/domain/scalaFxGui/SwingFunctionalFacade.java
@@ -0,0 +1,125 @@
+package domain.scalaFxGui;
+import javax.swing.*;
+import java.awt.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+
+class SwingFunctionalFacade {
+
+    public static interface Frame {
+        Frame setSize(int width, int height);
+        Frame addButton(String text, String name);
+        Frame addLabel(String text, String name);
+        Frame addComboBox(String[] items, String name);
+        String getSelectedComboBoxItem(String name);
+        Frame showToLabel(String text, String name);
+        Frame show();
+        Frame showAutomaton(String name);
+        Supplier<String> events();
+    }
+
+    public static Frame createFrame(){
+        return new FrameImpl();
+    }
+
+    private static class FrameImpl implements Frame {
+        private final JFrame jframe = new JFrame();
+        private final Map<String, JButton> buttons = new HashMap<>();
+        private final Map<String, JLabel> labels = new HashMap<>();
+        private final Map<String, JComboBox<String>> comboBoxes = new HashMap<>();
+        private final Map<String, JPanel> automatonPanels = new HashMap<>();
+        private String currentAutomaton = "";
+        private final LinkedBlockingQueue<String> eventQueue = new LinkedBlockingQueue<>();
+        private final Supplier<String> events = () -> {
+            try{
+                return eventQueue.take();
+            } catch (InterruptedException e){
+                return "";
+            }
+        };
+        public FrameImpl() {
+            this.jframe.setLayout(new FlowLayout());
+        }
+
+        @Override
+        public Frame setSize(int width, int height) {
+            this.jframe.setSize(width, height);
+            return this;
+        }
+
+        @Override
+        public Frame addButton(String text, String name) {
+            JButton jb = new JButton(text);
+            jb.setActionCommand(name);
+            this.buttons.put(name, jb);
+            jb.addActionListener(e -> {
+                try {
+                    eventQueue.put(name);
+                } catch (InterruptedException ex){}
+            });
+            this.jframe.getContentPane().add(jb);
+            return this;
+        }
+
+        @Override
+        public Frame addLabel(String text, String name) {
+            JLabel jl = new JLabel(text);
+            this.labels.put(name, jl);
+            this.jframe.getContentPane().add(jl);
+            return this;
+        }
+
+        @Override
+        public Frame addComboBox(String[] items, String name) {
+            JComboBox<String> comboBox = new JComboBox<>(items);
+            comboBox.setName(name);
+            this.comboBoxes.put(name, comboBox);
+            comboBox.addActionListener(e -> {
+                try {
+                    eventQueue.put(name + "Selected");
+                } catch (InterruptedException ex) {}
+            });
+            this.jframe.getContentPane().add(comboBox);
+            return this;
+        }
+
+        @Override
+        public String getSelectedComboBoxItem(String name) {
+            JComboBox<String> comboBox = this.comboBoxes.get(name);
+            return (String) comboBox.getSelectedItem();
+        }
+
+        @Override
+        public Supplier<String> events() {
+            return events;
+        }
+
+        @Override
+        public Frame showToLabel(String text, String name) {
+            this.labels.get(name).setText(text);
+            return this;
+        }
+
+        @Override
+        public Frame show() {
+            this.jframe.setVisible(true);
+            return this;
+        }
+
+        @Override
+        public Frame showAutomaton(String name) {
+            if (!this.automatonPanels.containsKey(name)) {
+                throw new IllegalArgumentException("An automaton with name " + name + " does not exist.");
+            }
+
+            if (!this.currentAutomaton.isEmpty()) {
+                this.automatonPanels.get(currentAutomaton).setVisible(false);
+            }
+            this.currentAutomaton = name;
+            this.automatonPanels.get(currentAutomaton).setVisible(true);
+            return this;
+        }
+
+    }
+}
