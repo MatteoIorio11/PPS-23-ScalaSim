841443981cf7c820f39f2e15dbf208467bf3f0b9
matteo.iorio2 <matteo.iorio2@studio.unibo.it>
2024-07-16 15:51:59 +0200

    refactor: remove Generic from Engine

--- scalasim/src/main/scala/domain/exporter/VideoExporter.scala
+++ scalasim/src/main/scala/domain/exporter/VideoExporter.scala
@@ -66,7 +66,7 @@ object JCodecVideoGenerator extends VideoGenerator {
 }
 
 object Exporter {
-  def exportMatrix[D <: Dimension, M, S <: State](engine: GeneralEngine[D, M], colors: Map[State, Color], converter: MatrixToImageConverter[D, M], videoGenerator: VideoGenerator, cellSize: Int, videoFilename: String, secondsPerImage: Double): Unit = {
+  def exportMatrix[D <: Dimension, M, S <: State](engine: GeneralEngine[D], colors: Map[State, Color], converter: MatrixToImageConverter[D, M], videoGenerator: VideoGenerator, cellSize: Int, videoFilename: String, secondsPerImage: Double): Unit = {
     val images = engine.history.zipWithIndex.map { case (matrix, _) =>
       converter.convert(matrix, cellSize, colors)
     }.toList
