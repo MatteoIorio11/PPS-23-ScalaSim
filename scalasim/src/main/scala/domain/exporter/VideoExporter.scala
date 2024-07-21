f6ad12e5dee3e5022efa5e8f80be8f341769f7a7
fabiovincenzi <fabio.vincenzi2001@gmail.com>
2024-07-21 14:45:31 +0200

    chore(javaDoc): add java doc for Video Exporter

--- scalasim/src/main/scala/domain/exporter/VideoExporter.scala
+++ scalasim/src/main/scala/domain/exporter/VideoExporter.scala
@@ -4,18 +4,32 @@ import domain.automaton.Cell
 import domain.automaton.CellularAutomaton.State
 import domain.base.Dimensions.{Dimension, TwoDimensionalSpace}
 import domain.engine.Engine.GeneralEngine
-import domain.simulations.gameoflife.GameOfLife.CellState
 import org.jcodec.common.io.{NIOUtils, SeekableByteChannel}
 import org.jcodec.common.model.Rational
 import org.jcodec.api.awt.AWTSequenceEncoder
 
 import java.awt.Color
 import java.awt.image.BufferedImage
-import domain.engine.Engine.GeneralEngine
+
+/**
+ * Trait for converting a matrix of cells to a BufferedImage.
+ *
+ * @tparam D the type of Dimension
+ */
 trait MatrixToImageConverter[D <: Dimension] {
+  /**
+   * Converts a matrix of cells to a BufferedImage.
+   *
+   * @param matrix        the matrix of cells
+   * @param cellSize      the size of each cell in pixels
+   * @param stateColorMap the mapping of cell states to colors
+   * @return the resulting BufferedImage
+   */
   def convert(matrix: LazyList[Cell[D]], cellSize: Int, stateColorMap: Map[State, Color]): BufferedImage
 }
-
+  /**
+   * Object for converting a matrix of cells in a TwoDimensionalSpace to a BufferedImage.
+   */
   object SimpleMatrixToImageConverter extends MatrixToImageConverter[TwoDimensionalSpace] {
     def convert(matrix: LazyList[Cell[TwoDimensionalSpace]], cellSize: Int, stateColorMap: Map[State, Color]): BufferedImage = {
       val (maxRow, maxCol) = matrix.foldLeft((0, 0)) {
@@ -39,11 +53,31 @@ trait MatrixToImageConverter[D <: Dimension] {
     }
   }
 
+/**
+ * Trait for generating a video from a sequence of BufferedImages.
+ */
 trait VideoGenerator {
+  /**
+   * Generates a video file from a sequence of BufferedImages.
+   *
+   * @param videoFilename   the name of the output video file
+   * @param images          the sequence of BufferedImages
+   * @param secondsPerImage the duration each image is displayed in the video
+   */
   def generate(videoFilename: String, images: Seq[BufferedImage], secondsPerImage: Double): Unit
 }
 
+/**
+ * Object for generating a video using the JCodec library.
+ */
 object JCodecVideoGenerator extends VideoGenerator {
+  /**
+   * Generates a video file from a sequence of BufferedImages using the JCodec library.
+   *
+   * @param videoFilename   the name of the output video file
+   * @param images          the sequence of BufferedImages
+   * @param secondsPerImage the duration each image is displayed in the video
+   */
   def generate(videoFilename: String, images: Seq[BufferedImage], secondsPerImage: Double): Unit =
     var out: SeekableByteChannel = null
     try
@@ -61,7 +95,23 @@ object JCodecVideoGenerator extends VideoGenerator {
       NIOUtils.closeQuietly(out)
 }
 
+/**
+ * Object for exporting a matrix of cells as a video file.
+ */
 object Exporter {
+  /**
+   * Exports a matrix of cells as a video file.
+   *
+   * @param engine          the engine containing the history of matrices
+   * @param colors          the mapping of cell states to colors
+   * @param converter       the converter for converting matrices to images
+   * @param videoGenerator  the generator for creating the video
+   * @param cellSize        the size of each cell in pixels
+   * @param videoFilename   the name of the output video file
+   * @param secondsPerImage the duration each image is displayed in the video
+   * @tparam D the type of Dimension
+   * @tparam S the type of State
+   */
   def exportMatrix[D <: Dimension, S <: State](engine: GeneralEngine[D], colors: Map[State, Color], converter: MatrixToImageConverter[D], videoGenerator: VideoGenerator, cellSize: Int, videoFilename: String, secondsPerImage: Double): Unit = {
     val images = engine.history.zipWithIndex.map { case (matrix, _) =>
       converter.convert(matrix, cellSize, colors)
